31a53496091f13032a888cc38eaba36d24c475b8
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-28 19:50:19 +0100

    Update

--- Adivina_el_número.py
+++ Adivina_el_número.py
@@ -1,5 +1,11 @@
+import random
+
 def adivina_el_numero(x):
     
     print('========================')
     print('¡Bienvenido(a) al juego!')
     print('========================')
+    print('Tu meta es adivinar el número generado por el ordenador.')
+    
+    numero_aleatorio = random.randint(1, x) # Número aleatorio entre 1 y x, ambos inclusive.
+    
