d846de64b29a3ac6081aec605ae02c4440970266
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-29 05:34:25 +0100

    Updated

--- Adivina_el_número.py
+++ Adivina_el_número.py
@@ -11,5 +11,7 @@ def adivina_el_numero(x):
     
     prediccion = 0
     
-    while
+    while prediccion != numero_aleatorio:
+        # El usuario ingresa un número
+        prediccion = int(input(f'Adivina un número entre 1 y {x}: ')) # f-string
     
