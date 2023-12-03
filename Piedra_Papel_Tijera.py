e7a490ec8c1d7b893a5c56610df3586515647302
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-12-03 08:55:52 +0100

    Updated

--- Piedra_Papel_Tijera.py
+++ Piedra_Papel_Tijera.py
@@ -5,3 +5,6 @@ def jugar():
     usuario = input("Escoge una opción: 'pi' para piedra, 'pa' para papel, 'ti' para tijera.\n").lower()
     computadora = random.choice(['pi', 'pa', 'ti'])
     
+    if usuario == computadora:
+        return '¡Empate!'
+    
