24331b83fd9a556eb80753e55b0bc12b11c1735d
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-12-05 17:48:37 +0100

    Updated

--- El_Ahorcado.py
+++ El_Ahorcado.py
@@ -1,7 +1,10 @@
+
+
+
 def ahorcado():
     
     print('====================================')
     print('¡Bienvenida(o) al juego del Ahorcado')
     print('====================================')
     
-    
+    palabra = obtener_palabra_valida(palabras)
