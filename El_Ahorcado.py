255f4dd753056e07bc12da3417ce6ffa41f26401
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-12-05 18:04:19 +0100

    Updated

--- El_Ahorcado.py
+++ El_Ahorcado.py
@@ -1,5 +1,15 @@
+import random
 
+from palabras import palabras
 
+def palabra = obtener_palabra_valida(palabras):
+    # Seleccionar una palabra al azar de la lista.
+    palabra = random.choice(palabras)
+    
+    while '-' in palabra or ' ' in palabra:
+        palabra = random.choice(palabras)
+        
+        return palabra.upper()
 
 def ahorcado():
     
