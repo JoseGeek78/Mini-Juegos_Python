5929869ba2cc9fcdbfbb2bc880755c09c3eab138
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-12-12 17:01:55 +0100

    Updated

--- busqueda_binaria.py
+++ busqueda_binaria.py
@@ -3,7 +3,11 @@ import time
 
 
 def búsqueda_ingenua(lista, objetivo):
-    for i in range(len(list)):
+    for i in range(len(lista)):
         if lista[i] == objetivo:
             return i
-    return -1    
+    return -1  
+
+
+mi_lista = [1, 3, 5, 10, 1 ]
+print(búsqueda_ingenua(mi_lista, 10))
